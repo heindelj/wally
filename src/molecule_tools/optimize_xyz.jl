f4a22f73d5d30dca58fabde0c88884bea885e9f1
Joe Heindel <heindelj@uw.edu>
2021-04-08 18:43:37 -0700

    Switched from @spawnat to pmap. Less allocation

--- src/molecule_tools/optimize_xyz.jl
+++ src/molecule_tools/optimize_xyz.jl
@@ -19,18 +19,15 @@ end
 function optimize_structures(structures::AbstractArray{Array{Float64,2}, 1}, potential::AbstractPotential; show_trace::Bool=false, copy_construct_potential=false)
     """
     Optimizes many guess structures suitable for the provided potential.
-    We divide the work among as many processes as possible.
+    We divide the work among as many processes as possible via parallel map.
     copy_construct_potential provides the option to copy construct the AbstractPotential as typeof(potential)(potential).
     This allows the use of potentials which are not thread safe, as is often the case with potentials written in fortran by scientists.
     """
-    future_results = Array{Future, 1}(undef, length(structures))
-    @sync for i in 1:length(structures)
-        if copy_construct_potential
-            future_results[i] = @spawnat :any optimize_xyz(structures[i], typeof(potential)(potential), show_trace=show_trace)
-        else
-            future_results[i] = @spawnat :any optimize_xyz(structures[i], potential, show_trace=show_trace)
-        end
+    if copy_construct_potential
+        paired_results = pmap(x -> optimize_xyz(x, typeof(potential)(potential), show_trace=show_trace), structures)
+    else
+        paired_results = pmap(x -> optimize_xyz(x, potential, show_trace=show_trace), structures)
     end
-    energies, geoms = collect(zip(fetch.(future_results)...))
-    return [energies...], [geoms...]
+    energies, geoms = collect(zip(paired_results...))
+    return energies, geoms
 end
