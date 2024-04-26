a9f783b38b438c1d2c2760860444ed83cce6b4ab
heindelj <heindelj@lbl.gov>
2024-04-26 13:15:01 -0700

    I guess progress bar needs eachindex?

--- src/molecule_tools/qchem_output_parsing.jl
+++ src/molecule_tools/qchem_output_parsing.jl
@@ -198,9 +198,9 @@ end
 function parse_final_geometry_from_output_files(output_files::Vector{String})
     all_labels = Vector{String}[]
     all_geoms = Matrix{Float64}[]
-    @showprogress for file in output_files
-        if occursin(".out", file)
-            labels, geoms = parse_geometries(file)
+    @showprogress for i in eachindex(output_files)
+        if occursin(".out", output_files[i])
+            labels, geoms = parse_geometries(output_files[i])
             push!(all_labels, labels[end])
             push!(all_geoms, geoms[end])
         end
